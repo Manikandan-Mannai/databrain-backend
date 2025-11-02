42cd6bfec60e1d2e06efc1e9396099dd98a67663
Manikandan-Mannai <iamanikandan.mannai@gmail.com>
2025-11-02 20:09:45 +0530

    updated cors() config

--- src/server.js
+++ src/server.js
@@ -14,7 +14,13 @@ dotenv.config();
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
